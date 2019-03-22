f2ece1c7af911202319d08d9b1200cd77c722cce
Roman Sologor <solohor.roman@gmail.com>
2019-03-22 21:18:08 +0100

    updated the comment list component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const CommentList = () => {
-  return (
-    <div>
-      Comment List
-    </div>
-  )
+class CommentList extends Component {
+  renderComments = () => (
+    this.props.comments.map((comment, index) => (
+      <li key={index}>
+        {comment}
+      </li>
+    ))
+  );
+
+  render() {
+    return (
+      <div>
+        {this.renderComments()}
+      </div>
+    );
+  }
 }
 
-export default CommentList;
+const mapStateToProps = state => ({
+  comments: state.comments,
+});
+
+export default connect(mapStateToProps)(CommentList);
