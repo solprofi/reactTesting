27340867201925c1f0437f55ae68ad30b5a05d86
Roman Sologor <solohor.roman@gmail.com>
2019-03-22 19:40:54 +0100

    refactored code for comment box test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,18 +16,31 @@ it('should render a text area and a button', () => {
   expect(wrapped.find('button').length).toBe(1);
 });
 
-it('should allow user to write in the textarea', () => {
-  const value = 'new comment';
-  wrapped.find('textarea').simulate('change', {
-    target: {
-      value,
-      name: 'comment'
-    }
+describe('the text area', () => {
+  let value;
+  beforeEach(() => {
+    value = 'new comment';
+    wrapped.find('textarea').simulate('change', {
+      target: {
+        value,
+        name: 'comment'
+      }
+    });
+    wrapped.update();
   });
 
-  wrapped.update();
+  it('should allow user to write in the textarea', () => {
+    expect(wrapped.find('textarea').prop('value')).toBe(value);
+  });
 
-  expect(wrapped.find('textarea').prop('value')).toBe(value);
+  it('should clear the textarea after the form is submitted', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toBe('');
+  });
 });
+
+
 
 
