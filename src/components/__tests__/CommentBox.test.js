7373f5459d4a6051dd6af7d120dc971cdd6ab7d7
Roman Sologor <solohor.roman@gmail.com>
2019-03-22 19:17:24 +0100

    added tests for comment box

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,18 @@ it('should render a text area and a button', () => {
   expect(wrapped.find('button').length).toBe(1);
 });
 
+it('should allow user to write in the textarea', () => {
+  const value = 'new comment';
+  wrapped.find('textarea').simulate('change', {
+    target: {
+      value,
+      name: 'comment'
+    }
+  });
+
+  wrapped.update();
+
+  expect(wrapped.find('textarea').prop('value')).toBe(value);
+});
+
+
